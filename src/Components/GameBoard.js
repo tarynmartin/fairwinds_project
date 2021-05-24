5e1bb9863cae3b5646008f26334dc31e900eca43
Taryn Martin <taryn.martin919@gmail.com>
2021-05-24 14:35:31 -0600

    Add improved logic and passing props to square

--- src/Components/GameBoard.js
+++ src/Components/GameBoard.js
@@ -2,30 +2,40 @@ import React from 'react';
 import Square from './Square';
 import './GameBoard.css';
 
-const GameBoard = () => {
+const GameBoard = ({ winningSquares, player, squares, setSquares, setCurrent }) => {
+  const createSquares = (i) => {
+    const squareClass = winningSquares? "no-click" : "square";
+    return (
+      <Square squareClass={squareClass} winningSquares={winningSquares} value={squares[i]} index={i} player={player} squares={squares} setSquares={setSquares} setCurrent={setCurrent}/>
+    )
+  }
 
   return (
-    <section class="game">
-      <div class="message">
-        <h1>hello!</h1>
+    <section className="game">
+      <div className="message">
+        {winningSquares && 
+          <h1>{player} WINS!</h1>
+        }
+        {!winningSquares && 
+          <h1>{player}'S TURN!</h1>
+        }
       </div>
-      <article class="gameboard">
-        <div class="row">
-          <Square />
-          <Square />
-          <Square />
+      <article className="gameboard">
+        <div className="row">
+          {createSquares(0)}
+          {createSquares(1)}
+          {createSquares(2)}
         </div>
-        <div class="row">
-          <Square />
-          <Square />
-          <Square />
+        <div className="row">
+          {createSquares(3)}
+          {createSquares(4)}
+          {createSquares(5)}
         </div>
-        <div class="row">
-          <Square />
-          <Square />
-          <Square />
+        <div className="row">
+          {createSquares(6)}
+          {createSquares(7)}
+          {createSquares(8)}
         </div>
-        {/* iterate & create 9 squares */}
       </article>
       {/* set conditional to render on win */}
       <div>
@@ -41,3 +51,7 @@ const GameBoard = () => {
 }
 
 export default GameBoard;
+
+// - on win, change winning backgrounds to green; display play again & see record buttons
+// - play again takes you to clear gameboard
+// - see record takes you to record screen
