ba9777787023f10b50c8a63f0e2df128e8488af2
Taryn Martin <taryn.martin919@gmail.com>
2021-05-24 14:35:48 -0600

    Add on click functionality for square

--- src/Components/Square.js
+++ src/Components/Square.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Square.css';
 
-const Square = () => {
+const Square = ({ squareClass, winningSquares, value, index, player, squares, setSquares, setCurrent }) => {
+
+  const className = squareClass;
+
+  const changeSquare = () => {
+    if (value === null) {
+      const splitSquares = squares.slice();
+      splitSquares[index] = player;
+      setSquares(splitSquares);
+      value = player;
+      if (!winningSquares){
+        player === 'X' ? setCurrent('O') : setCurrent('X');
+      }
+    }
+  }
+
   return (
-    <div>
-      {/* allow interpolation of strings when user clicks? */}
-      <h2>'Hello'</h2>
+    <div className={className} onClick={() => changeSquare()}>
+      <h2>{value}</h2>
     </div>
   )
 }
 
 export default Square;
+
+// - on win, change background to green
