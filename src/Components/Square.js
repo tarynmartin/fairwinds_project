859765e1881e41f595edfc9c5cd7c999a620b353
Taryn Martin <taryn.martin919@gmail.com>
2021-05-27 10:06:41 -0600

    Create possible code to display computer choice

--- src/Components/Square.js
+++ src/Components/Square.js
@@ -1,27 +1,73 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Square.css';
 
-const Square = ({ squareClass, winningSquares, value, index, player, squares, setSquares, setCurrent }) => {
+const Square = ({ computer, squareClass, winningSquares, value, index, player, squares, setSquares, setCurrent }) => {
 
+  // const [computerChoice, setChoice] = useState(null);
   const className = squareClass;
 
+  // const randomNum = () => {
+  //   return Math.floor(Math.random() * squares.length);
+  // }
+
+  // const findComputerChoice = () => {
+  //   console.log(squares)
+  //   if (player !== '' && player === computer) {
+  //     let choice = randomNum();
+  //     if (squares[choice] === null) {
+  //       setChoice(choice)
+  //     } else {
+  //       findComputerChoice()
+  //     }
+  //   }
+  //   console.log('computerChoice', computerChoice);
+  // }
+
+  // useEffect(() => {
+  //   findComputerChoice();
+  // }, [squares])
+
+  // const chooseComputerSquare = () => {
+  //   console.log(computerChoice)
+  //   if (computerChoice) {
+  //     const splitSquares = squares.slice();
+  //     splitSquares[computerChoice] = player;
+  //     setSquares(splitSquares);
+  //     value = player;
+  //     if (!winningSquares) {
+  //       player === 'X' ? setCurrent('O') : setCurrent('X');
+  //     }
+  //   }
+  // }
+
   const changeSquare = () => {
-    if (value === null) {
-      const splitSquares = squares.slice();
-      splitSquares[index] = player;
-      setSquares(splitSquares);
-      value = player;
-      if (!winningSquares){
-        player === 'X' ? setCurrent('O') : setCurrent('X');
-      }
+    console.log('player', player)
+    // console.log('computer', computer)
+    const splitSquares = squares.slice();
+    splitSquares[index] = player;
+    setSquares(splitSquares);
+    value = player;
+    if (!winningSquares){
+      player === 'X' ? setCurrent('O') : setCurrent('X');
     }
   }
 
+  // if (computerChoice) {
+  //   chooseComputerSquare();
+  // }
+
   return (
-    <div className={className} onClick={() => changeSquare()}>
-      <h2>{value}</h2>
+    <div>
+      {/* {(computerChoice && computerChoice === index) &&
+        <div className={className} onChange={() => console.log('here')} >
+          <h2>{player}</h2>
+        </div>
+      } */}
+      <div className={className} onClick={() => changeSquare()}>
+        <h2>{value}</h2>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Square;
